7c2add403a94475991b0446cf99a71ef8c317ac0
mgreen1092 <morgan.green0209@gmail.com>
2022-10-20 20:50:09 -0400

    everything but alerting its a tie

--- script.js
+++ script.js
@@ -12,59 +12,9 @@
 
 //two options for player one and player two
 
-let turns = 0
-
-function changeBackgroundColorP1 () {
-    turns = turns+1;
-    let message = document.querySelector('#message')
-    if (turns %2 === 0) {
-        this.style.backgroundColor = "blue";
-        message.innerText = "Player One's Turn!"
-        if (this.style.backgroundColor = "blue") {
-            alert('That space has already been selected! You miss your turn.')
-            this.disabled = "disabled";
-        }
-        // this.disabled = "disabled";
-    } else if (turns%2 != 0) {
-        this.style.backgroundColor = "green"
-        message.innerText = "Player Two's Turn!"
-        // this.disabled = "disabled";
-        if (this.style.backgroundColor = "green") {
-            alert('That space has already been selected! You miss your turn.')
-            this.disabled = "disabled";
-        }
-    }
-}
-
-
-//select player options
-let p1 = document.querySelector('#button1')
-let p2 = document.querySelector('#button2')
-//register player 1 click
-p1.addEventListener("click", function () {
-    p1.style.backgroundColor = "green"})
-//register player 2 click
-p2.addEventListener("click", function () {
-    p2.style.backgroundColor = "blue"
-})
-// function changeBackgroundColorP1 (event) {
-//     let boxSelection = []
-//     event.preventDefault()
-//     if (p1.style.backgroundColor === "blue") {
-//         this.style.backgroundColor = "blue";
-//         p1.style.backgroundColor = 'white'
-//         boxSelection.
-//     } else if (p2.style.backgroundColor === "green") {
-//         this.style.backgroundColor = "green"
-//         p2.style.backgroundColor = 'white'
-//     }
-// }
-
 
 const box1 = document.querySelector('#box1')
-box1.value = '0'
 const box2 = document.querySelector('#box2')
-box2.value = '1'
 const box3 = document.querySelector('#box3')
 const box4 = document.querySelector('#box4')
 const box5 = document.querySelector('#box5')
@@ -73,30 +23,77 @@ const box7 = document.querySelector('#box7')
 const box8 = document.querySelector('#box8')
 const box9 = document.querySelector('#box9')
 
-let boxArray = []
-boxArray.push(parseInt(box1.value))
-boxArray.push(parseInt(box2.value))
-console.log(boxArray)
-console.log(box1)
+let message = document.querySelector('#message')
 
+let playerTurn = 'x'
+let clickedBoxes = []
+let player1Wins = 0;
+let player2Wins = 0;
 
+function handleClick (event) {
+    event.preventDefault()
+    if (clickedBoxes.includes(event.target.id[3])) {
+        return;
+    }
+    // console.log(event.target.id[3])
+    clickedBoxes.push(event.target.id[3])
+    console.log(clickedBoxes)
+    if (playerTurn === 'x') {
+        event.target.style.backgroundColor = "blue"
+        message.innerText = "Player Two's Turn!"
 
-box1.addEventListener("click", changeBackgroundColorP1)
-box2.addEventListener("click", changeBackgroundColorP1)
-box3.addEventListener("click", changeBackgroundColorP1)
-box4.addEventListener("click", changeBackgroundColorP1)
-box5.addEventListener("click", changeBackgroundColorP1)
-box6.addEventListener("click", changeBackgroundColorP1)
-box7.addEventListener("click", changeBackgroundColorP1)
-box8.addEventListener("click", changeBackgroundColorP1)
-box9.addEventListener("click", changeBackgroundColorP1)
+     } 
+    if (playerTurn === 'o') {
+        event.target.style.backgroundColor = "green"
+        message.innerText = "Player One's Turn!"
+    }
+    //check to see what turn it is
+   
+    //check to see what turn it is
+    console.log(playerTurn)
+    playerTurn = (playerTurn === 'x' ? "o" : 'x')
+    //if player turn is eqaul to x, then change it to o, else change playerTurn to x
+    if ((box1.style.backgroundColor === 'blue' && box2.style.backgroundColor === 'blue' && box3.style.backgroundColor === 'blue') || 
+    (box4.style.backgroundColor === 'blue' && box5.style.backgroundColor === 'blue' && box6.style.backgroundColor === 'blue') || 
+    (box7.style.backgroundColor === 'blue' && box8.style.backgroundColor === 'blue' && box9.style.backgroundColor === 'blue')|| 
+    (box1.style.backgroundColor === 'blue' && box4.style.backgroundColor === 'blue' && box7.style.backgroundColor === 'blue') || 
+    (box2.style.backgroundColor === 'blue' && box5.style.backgroundColor === 'blue' && box8.style.backgroundColor === 'blue') || 
+    (box3.style.backgroundColor === 'blue' && box6.style.backgroundColor === 'blue' && box9.style.backgroundColor === 'blue') || 
+    (box1.style.backgroundColor === 'blue' && box5.style.backgroundColor === 'blue' && box9.style.backgroundColor === 'blue') || 
+    (box3.style.backgroundColor === 'blue' && box5.style.backgroundColor === 'blue' && box7.style.backgroundColor === 'blue')) {
+        setTimeout(function(){ 
+            alert("Player one wins!") 
+        }, 100);
+        player1Wins += 1;
+        let playerOneScore = document.querySelector('#player-one')
+        playerOneScore.innerText = player1Wins;
+        return;
+    } else if ((box1.style.backgroundColor === 'green' && box2.style.backgroundColor === 'green' && box3.style.backgroundColor === 'green') || 
+    (box4.style.backgroundColor === 'green' && box5.style.backgroundColor === 'green' && box6.style.backgroundColor === 'green') || 
+    (box7.style.backgroundColor === 'green' && box8.style.backgroundColor === 'green' && box9.style.backgroundColor === 'green')|| 
+    (box1.style.backgroundColor === 'green' && box4.style.backgroundColor === 'green' && box7.style.backgroundColor === 'green') || 
+    (box2.style.backgroundColor === 'green' && box5.style.backgroundColor === 'green' && box8.style.backgroundColor === 'green') || 
+    (box3.style.backgroundColor === 'green' && box6.style.backgroundColor === 'green' && box9.style.backgroundColor === 'green') || 
+    (box1.style.backgroundColor === 'green' && box5.style.backgroundColor === 'green' && box9.style.backgroundColor === 'green') || 
+    (box3.style.backgroundColor === 'green' && box5.style.backgroundColor === 'green' && box7.style.backgroundColor === 'green')) {
+        setTimeout(function(){ 
+            alert("Player two wins!") 
+        }, 100);
+        player2Wins += 1;
+        let playerTwoScore = document.querySelector('#player-two')
+        playerTwoScore.innerText = player2Wins
+        return;
+    }
+} 
+
+const divContainer = document.querySelector('.grid-container')
+divContainer.addEventListener("click", handleClick)
+
 
 //3: When the square is marked, user can't click on
 //it again
 
-
-
-//if box is not equal to an empty string??
+//completed with the includes method
 
 
 //4: A message should appear to show the players turn
@@ -114,9 +111,16 @@ const resetButton = document.querySelector('#reset-button')
 // console.log(resetButton)
 function reset () {
     //reset
-    window.location.reload(true)
+    let gridItems = document.querySelectorAll(".grid-item")
+    gridItems.forEach(gridItem => {
+        gridItem.style.backgroundColor = "white"
+    })
+    clickedBoxes = []
+    playerTurn = (playerTurn === 'x' ? "o" : 'x')
+    message.innerText = "Player One's Turn!"
+    } 
+    // window.location.reload(true)
     //https://stackoverflow.com/questions/42457544/trying-to-get-tic-tac-toe-board-to-reset
-}
 resetButton.addEventListener("click", reset);
 
 //===================================================
@@ -125,6 +129,18 @@ resetButton.addEventListener("click", reset);
 //GOAL: players dont have to manually score the game
 // - check the board with every turn 
 
+//Winning conditions: 
+//1, 2, 3
+//4, 5, 6
+//7, 8, 9
+//1, 4, 7
+//2, 5, 8
+//3, 6, 9
+//1, 5, 9
+//3, 5, 7
+//if background color is blue (player 1), player 1 wins else player 2 wins or tie
+
+
 //STEPS:
 //1: If a winning conidtion is met, display a message 
 //of the player that won 
@@ -132,5 +148,5 @@ resetButton.addEventListener("click", reset);
 //If all the squares are filled in and no one won, 
 //display a message saying its a tie
 
-//User should be able to reset the game 
+//User should be able to reset the game
 
